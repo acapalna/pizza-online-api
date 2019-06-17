ad10ef460c91d51406a145bf25665252eb63d8a8
augustin.capalna <augustin.capalna@yahoo.com>
2019-06-17 14:23:46 +0300

    Initial commit

---
+++ src/main/java/org/fasttrackit/pizzaonlineapi/PizzaOnlineApiApplication.java
@@ -0,0 +1,13 @@
+package org.fasttrackit.pizzaonlineapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaOnlineApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaOnlineApiApplication.class, args);
+	}
+
+}
